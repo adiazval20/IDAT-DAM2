10d567fd2461e0e47f7fa8339d6549af0c05e4c0
Andy <adiazval20@gmail.com>
2020-06-02 19:19:24 -0500

    2

---
+++ Semana9_2/app/src/main/java/edu/idat/semana9/DataActivity.java
@@ -0,0 +1,14 @@
+package edu.idat.semana9;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data);
+    }
+}
