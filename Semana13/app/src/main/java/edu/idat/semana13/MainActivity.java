9a005d538c5577f0694c6f8f26ffe4b2acd7a30d
Andy <adiazval20@gmail.com>
2020-06-30 19:25:08 -0500

    1

--- Semana13/app/src/main/java/edu/idat/semana13/MainActivity.java
+++ Semana13/app/src/main/java/edu/idat/semana13/MainActivity.java
@@ -1,14 +1,68 @@
 package edu.idat.semana13;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.app.TaskStackBuilder;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String CHANNEL_ID = "canal";
+    private Button btnLanzarNotificacion;
+    private PendingIntent pendingIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLanzarNotificacion = findViewById(R.id.btnLanzarNotificacion);
+        btnLanzarNotificacion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    mostrarNuevaNotificacion();
+                } else {
+                    mostrarNotificacion();
+                }
+            }
+        });
+    }
+
+    private void mostrarNuevaNotificacion() {
+        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "NEW", NotificationManager.IMPORTANCE_DEFAULT);
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        manager.createNotificationChannel(channel);
+        mostrarNotificacion();
+    }
+
+    private void mostrarNotificacion() {
+        setPendingIntent(HomeActivity.class);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_baseline_add_alert_24)
+                .setContentTitle("Mi primena notificación")
+                .setContentText("Esta es una notificación de prueba")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent);
+
+        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(getApplicationContext());
+        managerCompat.notify(1, builder.build());
+    }
+
+    private void setPendingIntent(Class<?> clsActivity) {
+        Intent intent = new Intent(this, clsActivity);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(this);
+        stackBuilder.addNextIntent(intent);
+        pendingIntent = stackBuilder.getPendingIntent(1, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 }
